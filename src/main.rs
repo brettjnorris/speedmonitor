50656326459b6d709322103e590a04ca101e41e0
Brett Norris <brettjnorris@gmail.com>
2020-07-11 19:12:14 -0400

    Parse contents of csv file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
-use std::{fs, io};
-use std::path::PathBuf;
-use std::fs::DirEntry;
+use std::{fs};
+use std::path::{PathBuf};
 use std::error::Error;
+
+use csv::{StringRecord, ReaderBuilder};
 
 fn main() {
     let path = env::current_dir().unwrap();
@@ -23,12 +24,12 @@ fn main() {
 struct Measurement {
     timestamp: String,
     server: String,
-    ping: u32,
+    ping: f32,
     download_rate: u32,
     upload_rate: u32,
 }
 
-fn find_files(path: PathBuf, query: String) -> Result<Vec<DirEntry>, Box<dyn Error>> {
+fn find_files(path: PathBuf, query: String) -> Result<Vec<String>, Box<dyn Error>> {
     let mut matches = vec![];
 
     for entry in fs::read_dir(path).unwrap() {
@@ -36,11 +37,10 @@ fn find_files(path: PathBuf, query: String) -> Result<Vec<DirEntry>, Box<dyn Err
         let path = entry.path();
 
         if !path.is_dir() {
-            let path = path.to_str().unwrap();
+            let path_str = path.to_str().unwrap();
 
-            if let true = path.contains(&query) {
-                println!("{:?}", path);
-                matches.push(entry);
+            if let true = path_str.contains(&query) {
+                matches.push(path_str.to_string());
             }
         }
     }
@@ -48,12 +48,34 @@ fn find_files(path: PathBuf, query: String) -> Result<Vec<DirEntry>, Box<dyn Err
     Ok(matches)
 }
 
-fn parse_contents(path: DirEntry) -> Result<Measurement, Box<dyn Error>> {
-    Ok(Measurement {
-        timestamp: String::from("2020-07-11T17:00:00"),
-        server: String::from("foo"),
-        ping: 0,
-        download_rate: 0,
-        upload_rate: 0
-    })
+fn parse_contents(path: String) -> Result<Vec<Measurement>, Box<dyn Error>> {
+    let mut measurements = vec![];
+    let mut rdr = ReaderBuilder::new().has_headers(false).from_path(path)?;
+
+    for result in rdr.records() {
+        let record = result?;
+        let measurement = parse_record(record)?;
+
+        measurements.push(measurement);
+    }
+
+    Ok(measurements)
+}
+
+fn parse_record(record: StringRecord) -> Result<Measurement, Box<dyn Error>> {
+    let timestamp = String::from("2020-07-11T17:00:00");
+    let server = record[0].to_string();
+    let ping = record[2].to_string().parse::<f32>().unwrap();
+    let download_rate = record[5].to_string().parse::<u32>().unwrap();
+    let upload_rate = record[6].to_string().parse::<u32>().unwrap();
+
+    let measurement = Measurement {
+        timestamp,
+        server,
+        ping,
+        download_rate,
+        upload_rate,
+    };
+
+    Ok(measurement)
 }
