c237467727c859f0fdaf5d800da89ba8197fd434
Brett Norris <brettjnorris@gmail.com>
2020-07-11 18:08:34 -0400

    Add Measurement struct

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,38 @@ use std::env;
 use std::{fs, io};
 use std::path::PathBuf;
 use std::fs::DirEntry;
+use std::error::Error;
 
 fn main() {
     let path = env::current_dir().unwrap();
-    println!("current_dir: {}", path.display());
+    let query = String::from(".csv");
 
-    let files = find_files(path).unwrap();
+    println!("current directory: {}", path.display());
+
+    let files = find_files(path, query).unwrap();
+
+    for file in files {
+        let measurement = parse_contents(file).unwrap();
+
+        println!("{:?}", measurement);
+    }
 }
 
-fn find_files(path: PathBuf) -> Result<Vec<DirEntry>, &'static str> {
+#[derive(Debug)]
+struct Measurement {
+    timestamp: String,
+    server: String,
+    ping: u32,
+    download_rate: u32,
+    upload_rate: u32,
+}
+
+fn find_files(path: PathBuf, query: String) -> Result<Vec<DirEntry>, Box<dyn Error>> {
     let mut matches = vec![];
 
     for entry in fs::read_dir(path).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
-
-        let query= String::from(".csv");
 
         if !path.is_dir() {
             let path = path.to_str().unwrap();
@@ -32,6 +48,12 @@ fn find_files(path: PathBuf) -> Result<Vec<DirEntry>, &'static str> {
     Ok(matches)
 }
 
-fn read_file(path: PathBuf) {
-
+fn parse_contents(path: DirEntry) -> Result<Measurement, Box<dyn Error>> {
+    Ok(Measurement {
+        timestamp: String::from("2020-07-11T17:00:00"),
+        server: String::from("foo"),
+        ping: 0,
+        download_rate: 0,
+        upload_rate: 0
+    })
 }
